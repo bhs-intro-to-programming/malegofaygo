092c84ae50a5095ab16875cddb94c049b181152b
malegofaygo <111394829+malegofaygo@users.noreply.github.com>
2022-09-12 15:06:36 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -7,7 +7,7 @@
  *
  * And these methods which do the same thing as in simple-draw.
  *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
+ *  drawLine(x1, y1, x2, y2, red, lineWidth)
  *
  *  drawCircle(x, y, radius, color, lineWidth=1)
  *
@@ -16,10 +16,10 @@
  *  drawTriangle(x1, y1, x2, y2, x3, y3, color, lineWidth=1)
  *
  *  drawFilledCircle(x, y, r, color)
- *
+
  *  drawFilledRect(x, y, width, height, color)
  *
  *  drawFilledTriangle(x1, y1, x2, y2, x3, y3, color)
  *
  *  clear()
- */
+ *
