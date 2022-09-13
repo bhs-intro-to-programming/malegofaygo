cc64827a3025b0a5753149305a87e1a891c6476b
malegofaygo <111394829+malegofaygo@users.noreply.github.com>
2022-09-13 14:41:04 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,7 +24,7 @@
  *  drawCircle (x1, y1, x2, y2, x3, y3, purple )
  */
 
-drawFilledCircle(50, 200, 300, 'red');
+drawFilledCircle(50, 200, 600, 'red');
 
 drawFilledCircle(100, 250, 70, 'blue');
 
