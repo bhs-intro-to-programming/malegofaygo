6bcab608ad963881860774b4f0ef4b75f861b7e7
malegofaygo <111394829+malegofaygo@users.noreply.github.com>
2022-11-02 15:11:01 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -12,7 +12,9 @@ const BLACK_KNIGHT = '♞';
 const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
-drawText(WHITE_KING, width/25, height/20, 'black', 45);
+drawText(WHITE_KING, width/25, height/20, 'BLACK', 45);
 
 drawText(BLACK_KING, width/30, height/25, 'WHITE', 55);
+
+drawText(White_KING, width/35, height/25, 'BLACK', 65);
  
